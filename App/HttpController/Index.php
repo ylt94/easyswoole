9d1d5d454e25046e371c941ca9d775068b1b7e67
litao <3@qq.com>
2019-10-28 17:01:12 +0800

    update

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -10,6 +10,7 @@ class Index extends Controller{
     public function index(){
         echo 'index/index request succ';
         $this->response()->write(json_encode(['status' => 1,'msg' => 'success']));
+        $this->response()->write(json_encode(['status' => 1,'msg' => 'success']));
         return true;
     }
 
