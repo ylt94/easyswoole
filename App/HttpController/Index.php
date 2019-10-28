8b303a4450e810a4775906f48df12efca722a304
litao <3@qq.com>
2019-10-28 16:17:53 +0800

    update

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -9,12 +9,12 @@ class Index extends Controller{
 
     public function index(){
         echo 'index/index request succ';
-        $this->response()->white(json_encode(['status' => 1,'msg' => 'success']));
+        $this->response()->write(json_encode(['status' => 1,'msg' => 'success']));
         return true;
     }
 
     public function test(){
-        $this->response()->white(json_encode(['status' => 1,'msg' => 'success']));
+        $this->response()->write(json_encode(['status' => 1,'msg' => 'success']));
         return true;
     }
 }
