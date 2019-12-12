428333b0124735eea01f337553d2fb2137f159bc
litao <3@qq.com>
2019-12-12 18:11:49 +0800

    update

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -16,10 +16,7 @@ class Index extends Controller{
 
     public function test(){
         usleep(100);
-        for($i = 1,$i < 8;$i++;){
-            echo 'success'.$i.PHP_EOL;
-        }
-        
+        echo 'success'.PHP_EOL;
         $this->response()->write(json_encode(['status' => 1,'msg' => 'success']));
         return true;
     }
